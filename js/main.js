aa6bcc8a91a3bb91d435f204e4c6f5beacbd76b4
Diego Augusto <diaugustobarros@gmail.com>
2020-01-23 20:37:21 -0200

    bottom nav fix

--- js/main.js
+++ js/main.js
@@ -2,3 +2,10 @@ $(".nav-links li").on("click", function(){
     $(".nav-links li").removeClass("active-top");
     $(this).addClass("active-top");
 });
+
+
+
+$(".bottom-navbar li").on("click", function(){
+    $(".bottom-navbar li").removeClass("active-bottom");
+    $(this).addClass("active-bottom");
+});
